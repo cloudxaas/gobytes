7c97542f925c011b71283a5e1cf83af186d43649
cloudxaas <cloudxaas@okit247.com>
2023-03-07 12:46:06 +0000

    123

--- bytes.go
+++ bytes.go
@@ -21,3 +21,39 @@ func Uint64ToBytes(n uint64) []byte {
 		byte(n),
 	}
 }
+
+func BytesToUint64(array []byte) uint64 {
+	var data uint64 = 0
+	if len(array) == 0 {
+		return 0
+	}
+	for i := 0; i < len(array); i++ {
+		data += uint64(uint(array[i]) << uint(8*(len(array)-i-1)))
+	}
+	return data
+}
+
+
+func BytesToUint16(array []byte) uint16 {
+	//return binary.BigEndian.Uint16(array)
+	if len(array) == 0 {
+		return 0
+	}
+	var data uint16 = 0
+	for i := 0; i < len(array); i++ {
+		data += uint16(uint(array[i]) << uint(8*(len(array)-i-1)))
+	}
+	return data
+
+}
+
+func BytesToUint32(array []byte) uint32 {
+	var data uint32 = 0
+	if len(array) == 0 {
+		return 0
+	}
+	for i := 0; i < len(array); i++ {
+		data += uint32(uint(array[i]) << uint(8*(len(array)-i-1)))
+	}
+	return data
+}
