05242e2f9628be142edbf0716af0e55415787b98
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-04 07:19:37 -0700

    Update bytes.go

--- bytes.go
+++ bytes.go
@@ -8,3 +8,31 @@ func BytesReverse(element []byte) []byte {
 }
 
 
+
+func BytesIncr(data []byte) []byte {
+	if len(data) == 0 {
+		return []byte{1}
+	}
+
+	// Find the index of the first non-zero byte from the right
+	idx := len(data) - 1
+	for idx >= 0 && data[idx] == 0 {
+		idx--
+	}
+
+	// If all bytes are 0, create a new byte slice with an extra leading 1
+	if idx < 0 {
+		b := make([]byte, len(data)+1)
+		b[0] = 1
+		return b
+	}
+
+	// Increment the last non-zero byte
+	element := make([]byte, len(data))
+	copy(element, data)
+	element[idx]++
+
+	return element
+}
+
+
