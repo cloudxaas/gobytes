2cf063bd12f1b9cb4ade9d3e7e650590526fc1d5
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-08 08:29:22 -0700

    Update int.go

--- int.go
+++ int.go
@@ -1,5 +1,45 @@
 package cxbytes
 
+func Int8ToBytesR(n int8) [1]byte {
+	uint8Value := uint8(n)
+	return [1]byte{
+		byte(uint8Value),
+	}
+}
+
+func Int16ToBytesR(n int16) [2]byte {
+	uint16Value := uint16(n)
+	return [2]byte{
+		byte(uint16Value),
+		byte(uint16Value >> 8),
+	}
+}
+
+func Int32ToBytesR(n int32) [4]byte {
+	uint32Value := uint32(n)
+	return [4]byte{
+		byte(uint32Value),
+		byte(uint32Value >> 8),
+		byte(uint32Value >> 16),
+		byte(uint32Value >> 24),
+	}
+}
+
+func Int64ToBytesR(n int64) [8]byte {
+	uint64Value := uint64(n)
+	return [8]byte{
+		byte(uint64Value),
+		byte(uint64Value >> 8),
+		byte(uint64Value >> 16),
+		byte(uint64Value >> 24),
+		byte(uint64Value >> 32),
+		byte(uint64Value >> 40),
+		byte(uint64Value >> 48),
+		byte(uint64Value >> 56),
+	}
+}
+
+
 func Int64ToBytes(n int) [8]byte {
 	uint64Value := uint64(n)
 	return [8]byte{
